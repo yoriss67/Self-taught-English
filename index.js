1a3ae16357d5e021ba3cee57a8e98b581dfb03fe
yoriss67 <109190795+yoriss67@users.noreply.github.com>
2022-10-12 11:48:58 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,263 @@
+
+
+// $(function firstAnimation() {
+//     $('.inview').on('inview', function (event, isInview) {
+//       if(isInview) {
+//         $(this).addClass('animate__animated animate__flip animate__infinite');
+//       } else {
+//         $(this).removeClass('animate__animated animate__flip animate__infinite');
+//         $(this).removeClass('inview');
+//       }
+//     });
+  
+//   });
+
+
+//   $(function(){
+//     if(firstAnimation) {
+//         $(this).addClass(second-inview);
+//     } else {
+//         $(this).removeClass();
+//     }
+//   })
+
+
+//   whenとdoneを使ってみたい
+
+// $(function() {
+    // $.when(
+
+        $(function firstAnimation() {
+            $('.inview').on('inview', function (event, isInview) {
+              if(isInview) {
+                $(this).removeClass('inview');
+                $(this).addClass('animate__animated animate__fadeInDown');
+              } else {
+                $(this).removeClass('animate__animated animate__fadeInDown');
+
+
+                // ????
+                $(this).addClass(inview);
+              }
+            });
+          
+          })    
+          
+          
+
+    // ) .done(function() {
+
+        // $(this).addClass(second-inview);
+
+
+//     })
+// })
+
+  
+
+
+// $(function() {
+//     $('.second-inview').on('inview', function(event, isInview) {
+//         if(isInview) {
+//             $(this).removeClass(second-inview);
+
+//         } else {
+//              $(this).addClass(second-inview);
+//         }
+//     })
+// });
+
+// teratail
+$(function () {
+  $(".row > div").on("inview", function (event, isInview) {
+    if (isInview) {
+      $(this).removeClass("second-inview");
+    } else {
+      $(this).addClass("second-inview");
+    }
+  });
+});
+
+
+
+
+// quizlet flashcard 入ってくるアニメーション
+
+$('#quizlet').on('inview', function(event, isInView) {
+  if (isInView) {
+    $('.flashcard1').addClass('animate__animated animate__fadeInUp ');
+  } else {
+    $('.flashcard1').removeClass(' animate__animated animate__fadeInUp ');
+  }
+});
+
+$('#quizlet').on('inview', function(event, isInView) {
+  if (isInView) {
+    $('.flashcard2').addClass('animate__animated animate__fadeInUp ');
+  } else {
+    $('.flashcard2').removeClass(' animate__animated animate__fadeInUp ');
+  }
+});
+
+$('#quizlet').on('inview', function(event, isInView) {
+  if (isInView) {
+    $('.flashcard3').addClass('animate__animated animate__fadeInUp ');
+  } else {
+    $('.flashcard3').removeClass(' animate__animated animate__fadeInUp');
+  }
+});
+
+$('#quizlet').on('inview', function(event, isInView) {
+  if (isInView) {
+    $('.flashcard4').addClass('animate__animated animate__fadeInUp');
+  } else {
+    $('.flashcard4').removeClass(' animate__animated animate__fadeInUp');
+  }
+});
+
+
+// lengthyすぎる！🤔🤔🤔🤔
+// let flashNum = $('.flashcard' + Number);
+
+// flashNum(1);
+
+
+
+
+
+
+
+// flashcard2 flip
+
+$('.flashcard2').click(function() {
+  // $(this).addClass('animate_animated animate__flipInY animate__faster');
+
+  $(this).toggleClass('animate_animated animate__flipInY animate__faster');
+  
+  if($('.flashcard2').text() === 'Fraternity / Sorority') {
+  $(this).text('男子・女子の社交クラブ');
+
+
+
+} else {
+
+  $(this).text('Fraternity / Sorority');
+
+}
+  // $('.head').toggle();
+  // $('.tail-opaci').toggle();
+
+
+});
+
+// flashcard 3 flip
+
+$('.flashcard3').click(function() {
+  // $(this).addClass('animate_animated animate__flipInY animate__faster');
+
+  $(this).toggleClass('animate_animated animate__flipInY animate__faster');
+
+if($('.flashcard3').text() == `Corn → "I have a foot corn on my left foot."`) {
+  $(this).text('魚の目');
+
+} else {
+  $(this).text(`Corn → "I have a foot corn on my left foot."`);
+
+}
+  // $('.head').toggle();
+  // $('.tail-opaci').toggle();
+
+
+});
+
+// できたバージョン
+// "Corn 'I have a foot corn on my left foot.'"
+
+
+
+
+
+
+
+
+
+
+// podcast
+
+// スマホ
+
+$(document).ready(function() {
+	$('.three-lines-menu').on('click', function() {	
+		$('#menu ul').toggleClass('menu-responsive');
+	});
+});
+
+
+
+
+
+
+
+
+// netflix
+// TOGGLE BUTTON
+
+$('#toggle').click(function() {
+
+  // var text = $('.english').text();
+
+if($('.english p').html() == 'Netflix is not only for entertainment, but also an appropriate material to learn real English. \<br> With a Chrome extension "Language Reactor", you\'re gonna immerse yourself into lively and latest world. Moreover, you can take a note on right side of the screen. Let\'s improve vocabulary and learn practical usage which isn\'t taught in school!') {
+  $('.english p').html('Netflixはエンターテイメントとしてだけでなく、本物の英語を学ぶための適切な教材でもあります。<br> Chrome拡張機能「Language Reactor」を使えば、生き生きとした最新の英語の世界に浸ることができます。また、画面右側にはメモを取ることができます。語彙を増やして、学校では学べない実用的な使い方を身につけましょう！');
+
+} else //if ($('.english').text() == 'この文は日本語です')//
+ {
+  $('.english p').html('Netflix is not only for entertainment, but also an appropriate material to learn real English. \<br> With a Chrome extension "Language Reactor", you\'re gonna immerse yourself into lively and latest world. Moreover, you can take a note on right side of the screen. Let\'s improve vocabulary and learn practical usage which isn\'t taught in school!');
+
+}
+
+// $(function() {
+//   $('.toggle-switch').click(function() {
+//     $('.english').toggleClass('japanese')
+//   })
+//   });
+  
+
+});
+
+// -------------------------
+// $('.flashcard2').click(function() {
+//   // $(this).addClass('animate_animated animate__flipInY animate__faster');
+
+//   $(this).addClass('animate_animated animate__flipInY animate__faster');
+
+// if($('.flashcard2').text() === 'Fraternity / Sorority') {
+//   $(this).text('男子・女子の社交クラブ');
+
+
+// } else {
+
+//   $(this).text('Fraternity / Sorority');
+
+// }
+//   // $('.head').toggle();
+//   // $('.tail-opaci').toggle();
+
+
+// });
+
+
+
+$(function() {
+$('.toggle-switch').click(function() {
+  $('.japanese').toggle();
+})
+});
+
+
+
+
+
+
+
+
+
